f0d26b2d7ac14b24f38dc391e63fcf9abb0c12a5
zhangtian <zhang.tian20080808>
2017-05-02 15:31:49 +0800

    redis分布式锁redlock

--- redis-distributed-lock-relock/src/main/java/cn/itcast/zt/Application.java
+++ redis-distributed-lock-relock/src/main/java/cn/itcast/zt/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * http://blog.csdn.net/forezp/article/details/70305336
+ * https://github.com/redisson/redisson/wiki/14.-Integration%20with%20frameworks#145-spring-session
+ * http://www.cnblogs.com/zhongkaiuu/p/redisson.html
  * Created by zhangtian on 2017/5/2.
  */
 @SpringBootApplication
