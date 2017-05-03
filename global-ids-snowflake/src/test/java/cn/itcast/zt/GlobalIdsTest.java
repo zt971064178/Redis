7509598cfd9e0ba836f740c17a4ff8cb2db6f5d5
zt971064178 <zhang.tian20080808@163.com>
2017-05-03 22:09:10 +0800

    Twitter的分布式自增ID算法snowflake (Java版)

---
+++ global-ids-snowflake/src/test/java/cn/itcast/zt/GlobalIdsTest.java
@@ -0,0 +1,17 @@
+package cn.itcast.zt;
+
+/**
+ * Created by Administrator on 2017/5/3/003.
+ */
+public class GlobalIdsTest {
+    //==============================Test=============================================
+    /** 测试 */
+    public static void main(String[] args) {
+        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
+        for (int i = 0; i < 1000; i++) {
+            long id = idWorker.nextId();
+            System.out.println(Long.toBinaryString(id));
+            System.out.println(id);
+        }
+    }
+}
